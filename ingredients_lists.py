ff9819cdd2f2d18a98caebcb29b9278f9226fc51
TonioDominguez <157376390+TonioDominguez@users.noreply.github.com>
2025-05-26 21:20:37 +0200

    ing[tipo_ingrediente] actualizado para orden lista

---
+++ ingredients_lists.py
@@ -0,0 +1,121 @@
+# Clasificación de carnes en blancas y rojas
+carns_blancas = [
+    # Pollo/Pollastre
+    "pollastre",
+    "pit de pollastre",
+    "pitrera de pollastre",
+    "cuixes de pollastre",
+    "pollastre a trossos amb os i pell",
+
+    # Pavo/Gall d'indi
+    "gall d'indi",
+    "pit de cal d'indi",
+]
+
+carns_vermellas = [
+    # Ternera/Vedella
+    "vedella",
+    "solomillo de vedella",
+    "filet de vedella",
+    "carn de vedella",
+
+    # Cerdo/Porc
+    "porc",
+    "costella de porc",
+    "escalops de llom de porc",
+    "pernil salat",
+    "pernil dolç",
+    "xoriço",
+    'cuixot salat',
+    'botifarrons',
+    'pernil dolç o salat',
+
+    # Carne genérica (generalmente se refiere a carne roja)
+    "carn picada",
+    "carn magra",
+    "carn de porc i vedella",
+]
+
+mariscos = [
+    "gambes",
+    "llagostins",
+    "almejas i mejillons (sense closques)"
+]
+
+peixos = [
+    "filets de lluç",
+    "lluç",
+    "bacallà",
+    "peix",
+    "salmó fresc",
+    "tonyina",
+    "llom de salmó",
+    "lloms de tonyina",
+    "llauna de sardines en oli",
+    "salmó fumat",
+    'daurada',
+    'calamar',
+    'anxova' 
+]
+
+verdures = [
+    'xampinyons', 'ceba blanca', 'alls', 'enciam romana', 'espinacs', 'espinacs frescs', 'enciam',
+    'tomàquets cherry', 'ceba morada', 'pastanaga', 'mongetes verdes', 'tomàquet', 'cogombre',
+    'pebrot', 'tomàquet triturat natural', 'coliflor', 'arròs de coliflor', 'bròquil',
+    'pebrot vermell', 'ceba', 'manat de bledes', 'ceba tendra', 'ceba dolça',
+    'escarola', "fulles d'api", 'bledes', "fulles d'enciam romana", 'kale o col', 'canonges o kale', 
+    'kale', 'tàperes', "fulles d'alfabrega", 'porro', 'cibulet', 'bolets', 'pebre verd', 'carxofes tendres', 
+    'edame', 'ruca', 'alberginia', 'moniato o camote', 'xirivia', 'cols de brusselles', 'carabassa', 
+    'manat de grells', 'patata', 'fulles verdes', 'espàrrecs', 'carabassí', 'llenties crues', 'carxofes',
+    'espàrrecs blancs', 'grell', 'amanida', 'col',
+]
+
+fruites = [
+    'alvocat', 'alvocat madur', 'coco ratllat', 'nabius', 'maduixes o fruits vermells', 'fruits vermells',
+    'platan madur', 'llimones', 'fraules',
+]
+
+làctics_i_ous = [
+    'iogurt grec', 'nata líquida', 'ous', 'formatge parmesà', 'llet sencera', 'llet de coco', 'formatge feta',
+    'formatge de cabra', 'formatge semicurat', 'iogurt natural sense sucre', 'formatge cheddar', 'formatge gouda',
+    'formatge mozzarella', 'formatge feta o cabra', 'formatge', 'formatge crema', 'llet', 'formatge de barra',
+    'formatge blau', "llet d'ametlla", 'formatge brie', 'kefir', 'mantega', 'mató', 'mantega aromatitzada',
+    'mantega sense sal', 'mozzarella',
+]
+
+condiments = [
+    'canyella', 'julivert', "oli d'oliva", 'sal i pebre', 'vinagre', 'fulla de llorer',
+    'comí en pols', 'pebre vermell dolç', 'romaní fresc', 'canyella en pols',
+    'pebre vermell picant', 'salsa tamari', 'gingebre en pols', 'farigola',
+    'gingebre fresc', 'cúrcuma en pols', 'orenga sec', 'orenga fresc',
+    'oli de coco', 'farigola fresca', 'mostassa dijon', 'sal gruixuda', 'espècies',
+    'comí', 'all en pols', 'salsa de tomàtiga natural', 'pebre vermell dolc fumat',
+    'pebre de cirereta', 'gingebre mòlt', 'salsa de soja', 'cúrcuma fresca',
+    'vinagre de poma', 'herbes mediterranies', 'alfabaguera fresca o herba sana', 
+    "fulles d'alfabrega fresca", 'bitxo o cayena en pols', 'coriandre o menta fresca',
+    'nou moscada', 'oregano sec', 'tomí sec', 'moraduix', 'fulles de coriandre', 'coriandre',
+]
+
+altres = [
+    'cacau nibs', 'aigua', 'llesca de pa de lli', 'llavors de lli mòltes', 'tofu ferm',
+    'farina de ametlles o de lli', 'cafe miso', 'brots de soja en conserva', 'maionesa', 'brou de verdures', 
+    'llavors de carbassa', 'xocolata 90%',
+    'cetogranola', 'pinyons', 'llevadura en pols (postres)', 
+    'ametlles', 'cacauets torrats',
+    'ametlles naturals', 'guindilla', 
+    "full d'alga nori / ortigues", 'cacau en pols sense sucre', 
+    'espaguetis konjac', 'eritritol',
+    'farina de ametlles', 'cafè de cúrcuma', 'lli mòlt', 
+    'flocs de civada', 'edulcorant', 'panses', 'brou de pollastre o peix', 
+    'guacamole', 'psillium', 'xía', 'pipes de girasol', 
+    'avellanes naturals', 'pasta de carabassí',
+    'crema de cacauats', 'tahin', 'fruits secs', 
+    'brou de carn', 
+    'bicarbonat de sodi', 'tallarins konjac', 'vi blanc', 'pesto',
+    'brou de verdures o pollastre',
+    'brou de peix', 'ametlles filetejades', 'postres de miso',
+    'nous', 'essència de vainilla', 'brou de pollastre', 
+    'cubs de llevadura fresca', 'pipes de carabassa', 'nous i ametlles',
+    'olives', 'olives negres', 'vi tinto', 'llavors de sèsam'
+]
+
